3ae300469edf2814f995c96860f18bc95cca253f
Arunesh Unnithan <aruneshdevs@gmail.com>
2023-12-29 01:31:46 +0530

    Changed the root point from App.js -> Viewport.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import AboutMe from './Components/AboutMe/AboutMe.js';
-import Landing from './Components/Landing/Landing.js';
-import { useState, useEffect } from 'react';
+import Viewport from './Components/Viewport/Viewport.js';
 
 function App() {
-  const [scrollDistance, setScrollDistance] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollDistance(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <div className="App">
-      <Landing scroll={scrollDistance}/>
-      {/* <AboutMe scroll={scrollDistance}/> */}
+      <Viewport />
     </div>
   );
 }
---
+++ src/Components/Viewport/Viewport.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import Landing from '../Landing/Landing';
+
+export default function Viewport() {
+    const [scrollDistance, setScrollDistance] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        setScrollDistance(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
+  return (
+    <div>
+      <Landing scrollDistance={scrollDistance} />
+    </div>
+  )
+}
